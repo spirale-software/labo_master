456783da9d53a68b370534bf11faecbc70905e5f
spirale-software <lapigerard@yahoo.fr>
2017-03-26 04:39:22 +0200

    Start developping package proposition

---
+++ Application/oneDotCom/app/daoImpl/UserDAO.java
@@ -0,0 +1,26 @@
+package daoImpl;
+
+import dao.IUserDAO;
+import models.User;
+
+
+public class UserDAO implements IUserDAO {
+
+	@Override
+	public Long addNewUser(User newUser) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User getUserById(Long idUser) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User getUserByCredentials(String email, String pwd) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
--- Application/oneDotCom/app/models/Role.java
+++ Application/oneDotCom/app/models/Role.java
@@ -1,20 +1,9 @@
 package models;
 
-public class Role {
-
-	private int id;
-	private String libelle;
+public enum Role {
 	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getLibelle() {
-		return libelle;
-	}
-	public void setLibelle(String libelle) {
-		this.libelle = libelle;
-	}
+	MEMBRE_FACULTAIRE,
+	RESP_CANAL,
+	RESP_COMMUNICATION,
+	PRESIDENT
 }
